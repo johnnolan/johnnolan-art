9fa457f298c74e1008a56d01aa79d331156a7baa
John Nolan <me@johnnolan.dev>
2020-11-16 21:23:39 +0000

    New content updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,19 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('css')
-    eleventyConfig.addPassthroughCopy('img')
-    eleventyConfig.addPassthroughCopy('*.xml')
-    eleventyConfig.addPassthroughCopy('*.png')
-    eleventyConfig.addPassthroughCopy('./**/*.jpeg')
-    eleventyConfig.addPassthroughCopy('*.ico')
-    eleventyConfig.addPassthroughCopy('*.webmanifest')
-    eleventyConfig.addPassthroughCopy('*.txt')
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('img')
+  eleventyConfig.addPassthroughCopy('*.xml')
+  eleventyConfig.addPassthroughCopy('*.png')
+  eleventyConfig.addPassthroughCopy('./**/*.jpg')
+  eleventyConfig.addPassthroughCopy('*.ico')
+  eleventyConfig.addPassthroughCopy('*.webmanifest')
+  eleventyConfig.addPassthroughCopy('*.txt')
 
-    eleventyConfig.addWatchTarget('./**/*.css')
+  eleventyConfig.addWatchTarget('./**/*.css')
 
-    return {
-      dir: { input: 'src', output: '_site', data: '_data' },
-      templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
-      htmlTemplateEngine: 'njk',
-      passthroughFileCopy: true
-    }
+  return {
+    dir: { input: 'src', output: '_site', data: '_data' },
+    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true
   }
+}
